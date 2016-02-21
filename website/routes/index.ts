1fde015d54aeaece590c96599b860ffde7db0594
josh <josh@joshtwigg.com>
2016-02-21 00:08:09 +0000

    Got charts drawing with some fake data.

--- website/routes/index.ts
+++ website/routes/index.ts
@@ -16,5 +16,100 @@ export function contact(req: express.Request, res: express.Response) {
 };
 
 export function stats(req: express.Request, res: express.Response) {
-    res.render('stats', { title: 'Stats', year: new Date().getFullYear(), message: 'Server stats page', statdata: { length: 4, stuff: ['one', 'two', 'three', 'four'] } });
+    var stuff = [
+        {
+            name: 'One',
+            data: [
+                {
+                    value: 300,
+                    color: "#F7464A",
+                    highlight: "#FF5A5E",
+                    label: "Red"
+                },
+                {
+                    value: 50,
+                    color: "#46BFBD",
+                    highlight: "#5AD3D1",
+                    label: "Green"
+                },
+                {
+                    value: 100,
+                    color: "#FDB45C",
+                    highlight: "#FFC870",
+                    label: "Yellow"
+                }
+            ]
+        },
+        {
+            name: 'Two',
+            data: [
+                {
+                    value: 30,
+                    color: "#F7464A",
+                    highlight: "#FF5A5E",
+                    label: "Red"
+                },
+                {
+                    value: 50,
+                    color: "#46BFBD",
+                    highlight: "#5AD3D1",
+                    label: "Green"
+                },
+                {
+                    value: 100,
+                    color: "#FDB45C",
+                    highlight: "#FFC870",
+                    label: "Yellow"
+                }
+            ]
+        },
+        {
+            name: 'Three',
+            data: [
+                {
+                    value: 90,
+                    color: "#F7464A",
+                    highlight: "#FF5A5E",
+                    label: "Red"
+                },
+                {
+                    value: 90,
+                    color: "#46BFBD",
+                    highlight: "#5AD3D1",
+                    label: "Green"
+                },
+                {
+                    value: 90,
+                    color: "#FDB45C",
+                    highlight: "#FFC870",
+                    label: "Yellow"
+                }
+            ]
+        },
+        {
+            name: 'Four',
+            data: [
+                {
+                    value: 30,
+                    color: "#F7464A",
+                    highlight: "#FF5A5E",
+                    label: "Red"
+                },
+                {
+                    value: 100,
+                    color: "#46BFBD",
+                    highlight: "#5AD3D1",
+                    label: "Green"
+                },
+                {
+                    value: 100,
+                    color: "#FDB45C",
+                    highlight: "#FFC870",
+                    label: "Yellow"
+                }
+            ]
+        }
+    ];
+
+    res.render('stats', { title: 'Stats', year: new Date().getFullYear(), message: 'Server stats page', statdata: { length: 4, stuff: stuff } });
 };
